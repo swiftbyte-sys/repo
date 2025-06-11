e23f7563473c637d5541bc3e30fa5b47820edb48
SwiftByte <47521980+swiftbyte-sys@users.noreply.github.com>
2025-06-11 09:49:40 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,13 @@ document.getElementById('contact-form').addEventListener('submit', function(even
   alert('Thank you for contacting SwiftByte! We will get back to you soon.');
   this.reset();
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.getElementById("hamburger");
+  const navMenu = document.getElementById("navMenu");
+
+  hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("show");
+  });
+});
